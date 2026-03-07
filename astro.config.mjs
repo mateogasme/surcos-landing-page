350f51cdae848112b5e79485a9bff984611b42cd
Figaronline <noreply.figaronline@gmail.com>
2026-03-07 14:29:08 -0300

    config: Update canonical site URL to naked domain and add www redirect.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.equiposurcos.com.ar',
+  site: 'https://equiposurcos.com.ar',
   integrations: [
     sitemap({
       i18n: {
